13d733afcc83345614ac47a9e28aef51cc5f4fe1
Salem <salem.dev.77@gmail.com>
2026-05-18 20:52:56 +0300

    chore: update launcher icons and configure CocoaPods integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,23 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'app/app.dart';
 import 'app/di.dart';
 
 void main() async {
   // Ensure Flutter engine bindings are initialized
   WidgetsFlutterBinding.ensureInitialized();
+  // Handle full screen splash on IOS
+  if (Platform.isIOS) {
+    SystemChrome.setEnabledSystemUIMode(
+      SystemUiMode.manual,
+      overlays: [SystemUiOverlay.bottom, SystemUiOverlay.top],
+    );
+  }
 
   // Initialize dependency injection container
   await initDI();
-  
 
   // Run the application
   runApp(const TareeqApp());
